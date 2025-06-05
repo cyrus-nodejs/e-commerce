39ab21e40854b7b4dd6f4312f837c756a4e6590e
king_cyrus <adeyemiemma45@gmail.com>
2025-06-05 22:04:33 +0100

    21th commit

--- server/global.d.ts
+++ server/global.d.ts
@@ -1,22 +1,21 @@
-
 import { Users, IFile } from '../server/types/@types';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import { Express } from 'express';
+
 declare global {
   var mongo: MongoMemoryServer;
+}
 
-  namespace Express {
-    interface Request {
-      user?: Users;
-       file?: IFile;
-      files?:IFile[];
-    }
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: Users;
+    file?: IFile;
+    files?: IFile[];
   }
+}
 
-  namespace cloudinary {
-    interface uploader {
-      upload: () => void;
-    }
+declare namespace cloudinary {
+  interface uploader {
+    upload: () => void;
   }
 }
 
