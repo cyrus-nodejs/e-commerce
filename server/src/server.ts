ec0feb4a5c468d64553b330cf94ab773861b50ca
king_cyrus <adeyemiemma45@gmail.com>
2025-07-10 17:13:23 +0100

    url commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -27,7 +27,7 @@ dotenv.config()
 
 // Cors configuration for server  Local host & web hosting services
 const corsOptions = {
-  origin:process.env.FRONTEND_URL2,
+  origin:process.env.FRONTEND_URL,
  credentials: true, 
  optionSuccessStatus: 200,
  methods: ['GET', 'PUT', 'POST', 'DELETE']
